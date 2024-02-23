751d0744ff175bc52520008262b066aa54e5fe9c
joshuawenata <joshuawenata@gmail.com>
2024-02-23 18:09:41 +0700

    dashboard frontend

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,17 @@ module.exports = {
   ],
   theme: {
     extend: {
+      colors: {
+        'background': '#E1F1FD',
+        'blues': '#170072',
+        'bluesa': '#4663AC',
+        'bluesk': '#9BB1D0',
+        'bluesi': '#FFFFFF',
+      },
+      fontFamily: {
+        'krona-one': ['"Krona One"', 'sans-serif'],
+        'luckiest-guy': ['"Luckiest Guy"', 'cursive'],
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
